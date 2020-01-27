c9496b3f6b616971bc44d9466cda944a0aa92258
hayato0426 <hayato.matsumura2103@gmail.com>
2020-01-27 18:47:09 +0900

    result

--- servo.py
+++ servo.py
@@ -6,19 +6,19 @@ GPIO.setup(2, GPIO.OUT)
 servo = GPIO.PWM(2, 50)
 servo.start(0.0)
 
-bottom = 2.5
-middle = 7.2
-top = 12.0
-
-for i in range(5):
-	servo.ChangeDutyCycle(bottom)
+def setservo(angle):
+	deg = ( 0.5 + (1.9 / 180) * angle )*5
+	servo.ChangeDutyCycle(deg)
 	time.sleep(1.0)
 
-	servo.ChangeDutyCycle(middle)
-	time.sleep(1.0)
 
-	servo.ChangeDutyCycle(top)
-	time.sleep(1.0)
+
+for i in range(3):
+	servo.ChangeDutyCycle(2.5)
+	time.sleep(1)
+	setservo(75)
+
+GPIO.cleanup()
 
 
 
